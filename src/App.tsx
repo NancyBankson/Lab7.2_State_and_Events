308ba89ac7bf981d85d614e484c85e2c9544f0da
Nancy Bankson <nbankson3@gmail.com>
2025-12-06 18:13:05 -0600

    statsdisplay and app.tsx updated

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import { TextInput } from './components/TextInput/TextInput';
 import type { TextInputProps } from './types';
+import { StatsDisplay } from './components/StatsDisplay/StatsDisplay';
 
 function App() {
   const [text, setText] = useState("");
@@ -11,13 +12,21 @@ function App() {
     initialValue: "",
     placeholder: "Start typing your content here...",
   }
-
+  const textStats1 = {
+    characterCount: 5,
+    wordCount: 0,
+    readingTime: 23
+  }
   return (
     <>
       <TextInput
         placeholder={initialText.placeholder}     
         initialValue={initialText.initialValue}
       onTextChange={initialText.onTextChange}
+      />
+      <StatsDisplay 
+        stats={textStats1}
+        showReadingTime={true}
       />
     </>
   )
---
+++ src/components/StatsDisplay/StatsDisplay.tsx
@@ -0,0 +1,21 @@
+import type { StatsDisplayProps } from "../../types";
+
+export function StatsDisplay({ stats, showReadingTime}: StatsDisplayProps) {
+
+    return (
+        <div id="container">
+            <div>
+                <p>Characters</p>
+                <h3>{stats.characterCount}</h3>
+            </div>
+            <div>
+                <p>Words</p>
+                <h3>{stats.wordCount}</h3>
+            </div>
+            <div>
+                <p>Reading Time</p>
+                { (showReadingTime) ? <h3>{stats.readingTime}</h3> : null }
+            </div>
+        </div>
+    )
+}
